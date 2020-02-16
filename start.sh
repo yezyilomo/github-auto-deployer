b2aadf25e03573b9604b384e75eff428ab174238
yezy <yezileliilomo@hotmail.com>
2020-02-16 03:08:11 +0300

    :sparkles:Add PORT option on start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PORT=6767;
 MY_SECRET=<Your Webhook Secret>;
 APP_NAME=github-auto-deployer;
 APP_DIR=~/.github-auto-deployer;
@@ -7,4 +8,8 @@ DEPLOYMENT_FILE=$APP_DIR/deployment.yml;
 
 
 # Start script(Do not edit it)
-MY_SECRET=$MY_SECRET DEPLOYMENT_FILE=$DEPLOYMENT_FILE  pm2 --name $APP_NAME  start node -- $APP_ENTRY_POINT;
+PORT=$PORT \
+MY_SECRET=$MY_SECRET \
+DEPLOYMENT_FILE=$DEPLOYMENT_FILE \
+pm2 --name $APP_NAME \
+start node -- $APP_ENTRY_POINT;
